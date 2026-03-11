6f5ae858f006392a4666e4a05c20de2ede4ac1d5
xCHZx <chzx94@gmail.com>
2026-03-10 23:08:27 -0500

    limpieza

--- dist/src/database/database.providers.d.ts
+++
@@ -1,3 +0,0 @@
-import { Provider } from '@nestjs/common';
-export declare const DATABASE_CONNECTION = "DATABASE_CONNECTION";
-export declare const databaseProviders: Provider[];
--- dist/src/database/schema/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './catalogs.schema';
-export * from './users.schema';
-export * from './businesses.schema';
-export * from './stamp_cards.schema';
-export * from './user_stamp_cards.schema';
-export * from './visits.schema';
